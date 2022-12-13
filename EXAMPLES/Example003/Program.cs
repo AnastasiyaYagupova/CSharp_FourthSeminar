0f8a2a029325c4345dbbc9be3ed9b3c07910d167
Anastasiya Yagupova <nastyayagupova@mail.ru>
2022-12-13 15:44:48 +0300

    add third example

--- EXAMPLES/Example003/Program.cs
+++ EXAMPLES/Example003/Program.cs
@@ -5,64 +5,64 @@
 [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
 */
 
-//int range = 1;
-//while(range <= 10)
-
-//int N = new Random().Next(10,1000);
-//Console.WriteLine(N);
-
-Console.WriteLine("Введите число ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-int Num = N;
-int Num2 = N;
-int Count = 0;
-    while(Num > 0)
-    {
-    Num = Num / 10;
-    Count++;
-    }
-
-int [] Array = new int [Count];
-
-    int i = Count;
-        while(Num2 > 0)
-        {
-            Array[i-1] = Num2 % 10; 
-            Num2 = Num2 / 10;  
-            i--;
-        }
-
-int sum = 0;
+int[] Array = new int[10];
 int index = 0;
-while(index < Count)
-{
-    sum = sum + Array[index];
-    index++;
-}
+Random random = new Random();
+            while (index < 10)
+            {
+                int z = random.Next(1,1000);
+                int Num = z;
+                int Num2 = z;
+                int Count = 0;
+                while(Num > 0)
+                {
+                    Num = Num / 10;
+                    Count++;
+                }
 
-int product = 1;
-int position = 0;
-while(position < Count)
-{
-    if(Array[position]>0)
-    {
-    product = product * Array[position];
-    position++;
-    }
-    else
-    {
-    position++;
-    }
-}
-int resultat = N;
-if(product % sum == 0)
-{
-    Console.WriteLine(resultat);
-}
-else
-{
-    Console.WriteLine("Нет");
-}
+                int [] Array2_0 = new int [Count];
+
+                int i = Count;
+                 while(Num2 > 0)
+                {
+                    Array2_0[i-1] = Num2 % 10; 
+                    Num2 = Num2 / 10;  
+                     i--;
+                }
+                int sum = 0;
+                int ind = 0;
+                while(ind < Count)
+                {
+                    sum = sum + Array2_0[ind];
+                    ind++;
+                }
+
+                int product = 1;
+                int position = 0;
+                while(position < Count)
+                {
+                    if(Array2_0[position]>0)
+                    {
+                        product = product * Array2_0[position];
+                        position++;
+                    }
+                    else
+                    {
+                        position++;
+                    }
+                }
+                if(product % sum == 0)
+                {
+                    Array[index] = z;
+                    index++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+var str = string.Join(" ", Array);
+Console.WriteLine($"10 интересных чисел: {str}");
 
 
